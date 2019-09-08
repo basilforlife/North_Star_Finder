5a96893c83e26a620eb0292a301629e1ee9d985e
Jonathan Roy <roy@alumni.mines.edu>
2019-09-07 18:36:44 -0600

    First CUDA code added

--- starimage.py
+++ starimage.py
@@ -8,25 +8,40 @@ import cv2
 import scipy.signal as signal
 import seaborn as sns
 from scipy.optimize import minimize
+from numba import cuda
 
 
 # Image class, where most of the functionality occurs
 class StarImage:
 
+    # configure to environment
+    device_available = False
+
     # define Sobel edge detection kernels
     x_edge_kernel = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]]) # horizontal edges
     y_edge_kernel = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]]) # vertical edges
-
+    if self.device_available: # copy sobel edge kernels to device
+        d_x_edge_kernel = cuda.to_device(x_edge_kernel) 
+        d_y_edge_kernel = cuda.to_device(y_edge_kernel)
+ 
     def __init__(self, path, filename):
         self.gray_img = cv2.imread(path + filename, cv2.IMREAD_GRAYSCALE) # get grayscale img
         self.height, self.width = self.gray_img.shape
         self.path = path
         self.filename = filename
+        if self.device_available: # copy sobel edge kernels and gray_img to device
+            self.d_gray_img = cuda.to_device(self.gray_img)
 
     # This fn makes the x and y edge detected versions of the image
     def convolve(self):
         self.x_edge_img = signal.convolve2d(self.gray_img, self.x_edge_kernel, mode='same')
         self.y_edge_img = signal.convolve2d(self.gray_img, self.y_edge_kernel, mode='same') 
+
+    # This fn makes the x and y edge detected versions of the image
+    def cuda_convolve(self):
+        self.d_x_edge_img = signal.convolve2d(self.gray_img, self.x_edge_kernel, mode='same')
+        self.d_y_edge_img = signal.convolve2d(self.gray_img, self.y_edge_kernel, mode='same') 
+
 
     # This fn removes edges facing up, so that each star track only gives one edge. Optional to use
     def removeUpwardEdges(self):
@@ -40,6 +55,15 @@ class StarImage:
     def makePolar(self):
         self.magnitude_img = np.sqrt(self.x_edge_img**2 + self.y_edge_img**2)
         self.orientation_img = np.arctan2(self.x_edge_img, self.y_edge_img) + np.pi
+
+    # This fn makes the polar coords of the edge vectors of the image
+    @cuda.jit
+    def cuda_makeMagnitude(self):
+        start = cuda.grid(1)
+        stride = cuda.gridsize(1)
+        self.d_magnitude_img = cuda.device_array( # Fill this in with correct size !!!!!!!!!!!!!!!!!!!!!!
+        for i in range(start, self.d_gray_img.shape[0]:
+            self.d_magnitude_img[i] = math.sqrt(self.d_x_edge_img[i]**2 + self.d_y_edge_img[i]**2)
 
     # This fn makes an array in the shape of gray_img where the values match the indices, for use in the cost fn 
     # This could be accomplished by using the same for loop structure in the cost fn,
